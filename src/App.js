c9a778d82c36cf7f5266abb1b3a805f165e9abcf
moonishkale <manishkhadgi123@gmail.com>
2023-03-26 11:32:25 +1100

    setup frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Button } from "react-bootstrap";
+
+import { toast, ToastContainer } from "react-toastify";
 import "./App.css";
 
 function App() {
+  const notify = () => toast.status("Very easy");
+
   return (
     <div className="App">
-      <Button>Test</Button>
-      hellow
+      <Button onClick={notify}> Test</Button>
+
+      <ToastContainer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
